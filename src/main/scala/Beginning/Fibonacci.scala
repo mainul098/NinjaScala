ada2b4ea907b7ef58cc8db606bcdb6587a4b45cd
Mainul Islam <mainul098@gmail.com>
2017-02-15 00:29:06 +0600

    Update Fibonacci.scala

--- src/main/scala/Beginning/Fibonacci.scala
+++ src/main/scala/Beginning/Fibonacci.scala
@@ -3,6 +3,11 @@ package Beginning
 /**
   * Created by mislam on 2/13/2017.
   */
-class Fibonacci {
+object Fibonacci {
+  def fib(n: Int): Int = {
+    def fibIteration(i: Int, a: Int, b: Int): Int =
+      if (i == n) a else fibIteration(i + 1, b, a + b)
 
+    fibIteration(0, 0, 1)
+  }
 }
